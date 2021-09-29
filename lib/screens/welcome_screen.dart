449747ceb2bc87f9529d3c6d1a8027cc5fcc1fbd
Manas Karingal <manas.karingal@gmail.com>
2021-09-29 17:33:45 -0400

    background for welcome screen working

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,7 +1,7 @@
-import 'dart:ui';
-
 import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:limit_score/components/rounded_button.dart';
 import 'package:limit_score/screens/registration_screen.dart';
 
@@ -49,67 +49,85 @@ class _WelcomeScreenState extends State<WelcomeScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: animation.value,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
+      //backgroundColor: animation.value,
+      body: Stack(
+        children: [
+          Container(
+            height: double.infinity,
+            child: Image(
+              image: AssetImage('assets/images/Illustration1.png'),
+              fit: BoxFit.fill,
+            ),
+          ),
+          Container(
+            height: double.infinity,
+            child: Image(
+              image: AssetImage('assets/images/Illustration4.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
-                Expanded(
-                  child: Align(
-                    alignment: Alignment.centerRight,
-                    child: Hero(
-                      tag: 'logo',
-                      child: Icon(
-                        Icons.credit_score_outlined,
-                        size: 60.0,
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Expanded(
+                      child: Align(
+                        alignment: Alignment.centerRight,
+                        child: Hero(
+                          tag: 'logo',
+                          child: Icon(
+                            Icons.credit_score_outlined,
+                            size: 60.0,
+                          ),
+                        ),
                       ),
                     ),
-                  ),
-                ),
-                Expanded(
-                  flex: 2,
-                  child: Padding(
-                    padding: EdgeInsets.only(left: 15.0),
-                    child: AnimatedTextKit(
-                      animatedTexts: [
-                        TypewriterAnimatedText(
-                          'Limit Score',
-                          textStyle: const TextStyle(
-                            fontSize: 45.0,
-                            fontWeight: FontWeight.w900,
-                          ),
-                          speed: const Duration(milliseconds: 200),
+                    Expanded(
+                      flex: 2,
+                      child: Padding(
+                        padding: EdgeInsets.only(left: 15.0),
+                        child: AnimatedTextKit(
+                          animatedTexts: [
+                            TypewriterAnimatedText(
+                              'Limit Score',
+                              textStyle: const TextStyle(
+                                fontSize: 45.0,
+                                fontWeight: FontWeight.w900,
+                              ),
+                              speed: const Duration(milliseconds: 200),
+                            ),
+                          ],
                         ),
-                      ],
+                      ),
                     ),
-                  ),
+                  ],
+                ),
+                SizedBox(
+                  height: 48.0,
+                ),
+                RoundedButton(
+                  title: 'Log In',
+                  color: Colors.black,
+                  onPressed: () {
+                    Navigator.pushNamed(context, LoginScreen.id);
+                  },
+                ),
+                RoundedButton(
+                  title: 'Register',
+                  color: Colors.black,
+                  onPressed: () {
+                    Navigator.pushNamed(context, RegistrationScreen.id);
+                  },
                 ),
               ],
             ),
-            SizedBox(
-              height: 48.0,
-            ),
-            RoundedButton(
-              title: 'Log In',
-              color: Colors.black,
-              onPressed: () {
-                Navigator.pushNamed(context, LoginScreen.id);
-              },
-            ),
-            RoundedButton(
-              title: 'Register',
-              color: Colors.black,
-              onPressed: () {
-                Navigator.pushNamed(context, RegistrationScreen.id);
-              },
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
